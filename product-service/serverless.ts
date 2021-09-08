aed29b22e2f122ab65949b8eb94baa0fba7e081f
Katerina Dotsenko <ekatrinadotsenko@gmail.com>
2021-09-08 16:36:48 +0200

    feat: implement createProduct lambda function

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -2,6 +2,7 @@ import type { AWS } from '@serverless/typescript';
 
 import getProductsList from '@functions/getProductsList';
 import getProductsById from '@functions/getProductsById';
+import createProduct from '@functions/createProduct';
 
 const serverlessConfiguration: AWS = {
   service: 'product-service',
@@ -32,7 +33,7 @@ const serverlessConfiguration: AWS = {
     lambdaHashingVersion: '20201221',
   },
   // import the function via paths
-  functions: { getProductsList, getProductsById },
+  functions: { getProductsList, getProductsById, createProduct },
 };
 
 module.exports = serverlessConfiguration;
---
+++ product-service/src/functions/createProduct/index.ts
@@ -0,0 +1,14 @@
+import { handlerPath } from '@libs/handlerResolver';
+
+export default {
+  handler: `${handlerPath(__dirname)}/handler.createProduct`,
+  events: [
+    {
+      http: {
+        method: 'post',
+        path: 'products',
+        cors: true
+      }
+    }
+  ]
+}
